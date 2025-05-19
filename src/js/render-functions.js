5743f3714c409f0fdd8df436fd960dfcd8f3765c
Oksana <omoskovko98@gmail.com>
2025-05-19 15:27:56 +0300

    task accomplished

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,6 +1,43 @@
 // SimpleLightbox
+const gallery = document.querySelector(".gallery");
 
-// function createGallery(images)
-// function clearGallery()
-// function showLoader()
-// function hideLoader()
+export function createGallery(hits) {
+
+  const markup = hits
+    .map((img) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${img.largeImageURL}">
+          <img 
+            class="gallery-image" 
+            src="${img.webformatURL}" 
+            alt="${img.tags}" 
+          />
+        </a>
+        <div class="information"> 
+        <p><span class="bold-font">Likes:</span> ${img.likes}</p>
+        <p><span class="bold-font">Views:</span> ${img.views}</p>
+        <p><span class="bold-font">Comments:</span> ${img.comments}</p>
+        <p><span class="bold-font">Downloads:</span> ${img.downloads}</p>
+        </div>
+        
+      </li>
+    `)
+        .join("");
+    
+  gallery.innerHTML = markup;
+}
+
+
+export function clearGallery() {
+    const galleryItem = document.querySelector(".gallery-item");
+    gallery.removeAttribute(galleryItem);
+}
+
+export function showLoader() {
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'block';
+}
+export function hideLoader() {
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'none';
+}
--- src/main.js
+++ src/main.js
@@ -1,27 +1,60 @@
 import { getImagesByQuery } from './js/pixabay-api.js'
+import { createGallery } from './js/render-functions.js'
+import { clearGallery } from './js/render-functions.js'
+import { showLoader } from './js/render-functions.js'
+import { hideLoader } from './js/render-functions.js'
 
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-// Описаний у документації
+
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 //
 const form = document.querySelector('.form');
 const input = document.querySelector('input[name="search-text"]')
+const gallery = document.querySelector(".gallery");
 //
-
 form.addEventListener('submit', e => {
   e.preventDefault();
   const query = input.value.trim();
-    if (!query) {
-        iziToast.error({
-            message: 'Sorry, there are no images matching your search query. Please try again!',
-        });
+
+  if (!query) {
+    iziToast.error({
+      message: 'Sorry, there are no images matching your search query. Please, try again!',
+    });
     return;
     }
     
-    getImagesByQuery(query);
+    
+    showLoader();
+    
+    getImagesByQuery(query)
+    .then(data => {
+        hideLoader();
+        const hits = data.hits;
+
+      if (!hits.length) {
+        iziToast.error({
+          message: 'Sorry, there are no images matching your search query. Please, try again!',
+        });
+        return;
+      }
+
+      createGallery(hits);
+
+      const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+
+      lightbox.refresh();
+    })
+    .catch(error => {
+        hideLoader();
+      iziToast.error({
+        message: 'Something went wrong. Please try again later.',
+      });
+    });
 });
